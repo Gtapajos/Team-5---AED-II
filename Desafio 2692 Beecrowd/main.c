5cd6ca2081cbe9793edf1dea814fa786b8c43c61
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2023-03-03 15:36:46 -0300

    add 2692

--- Desafio 2692 Beecrowd/main.c
+++ Desafio 2692 Beecrowd/main.c
@@ -1,34 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
-int main() {
-    int num_changes, num_words, i, j;
-    char old_char, new_char;
-    scanf("%d %d", &num_changes, &num_words);
+#define ASIZE 256
 
-    // create a dictionary of changes to be made
-    char changes[256] = {0}; // initialize the array to 0
-    for (i = 0; i < num_changes; i++) {
-        scanf(" %c %c", &old_char, &new_char);
-        changes[(unsigned char)old_char] = new_char;
-        changes[(unsigned char)new_char] = old_char;
-    }
+struct RaitaAlgorithm {
+int resultIndex[100];
+int count;
+};
 
-    // loop through each word and make necessary changes
-    char words[num_words][100]; // assuming maximum word length is 100 characters
-    for (i = 0; i < num_words; i++) {
-        scanf(" %[^\n]", words[i]);
-        for (j = 0; j < strlen(words[i]); j++) {
-            if (changes[(unsigned char)words[i][j]]) {
-                words[i][j] = changes[(unsigned char)words[i][j]];
-            }
-        }
-    }
+int floorDivAlt(int x, int y) {
+  int r = x / y;
+  if ((x ^ y) < 0 && (r * y != x)) {
+  r--;
+  }
+  return r;
+}
+
+void preprocessing(char* pattern, int m, int* bmBc) {
+  for (int i = 0; i < ASIZE; i++) {
+  bmBc[i] = m;
+  }
+
+  for (int i = 0; i < m - 1; i++) {
+  bmBc[(int)pattern[i]] = m - i - 1;
+  }
+  }
+
+bool search(struct RaitaAlgorithm* ra, char* pattern, char* descript, char* text) {
+  int m = strlen(pattern);
+  int n = strlen(text);
+  char caracter = pattern[0];
+  int bmbc[ASIZE];
+  printf("desc: %s\n", descript);
+  preprocessing(pattern, m, bmbc);
+
+  bool result = false;
+  int j = 0;
   
-   for (i = 0; i < num_words; i++) {
-        printf("%s\n", words[i]);
+  while (j <= (n - m)) {
+      if (pattern[0] == text[j]) {
+        ra->resultIndex[ra->count++] = j;
+        text[j] = descript[0];
+        result = true;
+      }
+      else if(descript[0] == text[j]){
+            text[j] = pattern[0];
+      }
+    j += bmbc[(int)text[j + m - 1]];
+  }
+
+return result;
+}
+
+int main() {
+
+    struct RaitaAlgorithm ra = {.count = 0};
+    // w l
+    // s m
+    // e o
+    // a b
+    // c z
+    char str[] =  "mbs loww 2017"; //"sam well 2017"
+    char patterns[5] = "lmobz";
+    char descripts[5] = "wseac";
+    int size = 5;
+    for(int j = 0; j < size; j++){
+      char pattern[2] = {patterns[j], '\0'};
+      char desc[2] = {descripts[j], '\0'};
+      bool result = search(&ra, pattern, desc, str);
+      //printf("\nresultado: %d", result);
+    //   if (result) {
+    //       for (int i = 0; i < ra.count; i++) {
+    //           //printf("\n %c == %c", str[ra.resultIndex[i]], descripts[j]);
+    //           str[ra.resultIndex[i]] = descripts[j];
+    //       }
+    //   }
+      
+    //   ra.count = 0;
+    //   result = false;
     }
+
+    printf("\n%s\n", str);
 
     return 0;
-}
+  }
